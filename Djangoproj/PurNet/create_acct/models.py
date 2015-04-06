642c79aaeb83e396caccdc1f49681b9f3913662c
Stephen Kercher <skercher@purdue.edu>
2015-04-06 13:35:58 -0400

    Updated Databases and Models to easier use authentication.

--- Djangoproj/PurNet/create_acct/models.py
+++ Djangoproj/PurNet/create_acct/models.py
@@ -2,14 +2,16 @@ from django.db import models
 import datetime
 from django.utils import timezone
 from course_mang.models import Course
+from django.contrib.auth.models import User
 
 
 
-class User(models.Model):
-	name = models.CharField(max_length=50)
-	username = models.CharField(max_length=16, unique=True)
-	email = models.CharField(max_length=30)
-	password = models.CharField(max_length=40)
+class Site_User(models.Model):
+	user = models.OneToOneField(User)
+# 	name = models.CharField(max_length=50)
+# 	username = models.CharField(max_length=16, unique=True)
+# 	email = models.CharField(max_length=30)
+# 	password = models.CharField(max_length=40)
 	courses=models.ManyToManyField(Course)
 	def __str__(self):
 		return str(self.id)
