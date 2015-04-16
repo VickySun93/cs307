d631dde718f4dd8c3efb2f9788abbb37e1536d02
Di Pan <pandi.danny.cn@gmail.com>
2015-04-16 18:54:14 -0400

    new homepage folder added

---
+++ Djangoproj/PurNet/purdue/purdue/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'purdue.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^$', 'purdue.views.login', name='login'),
+    url(r'^logout/', 'purdue.views.logout', name='logout'),
+)
