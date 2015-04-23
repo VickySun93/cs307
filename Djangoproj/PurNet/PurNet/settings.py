8490323ffc3ceb16332f70e2e128a5f911e00f58
Di Pan <pandi.danny.cn@gmail.com>
2015-04-23 18:37:30 -0400

    homepage fixed

--- Djangoproj/PurNet/PurNet/settings.py
+++ Djangoproj/PurNet/PurNet/settings.py
@@ -88,7 +88,7 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
+#filepath that will hold user-uploaded files
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 # Static files (CSS, JavaScript, Images)
