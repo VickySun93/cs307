1c5965f532803d028869f423bdb2e74d5f4ce0cf
Di Pan <pandi.danny.cn@gmail.com>
2015-04-29 19:15:41 -0400

    add button finished

--- Djangoproj/PurNet/course_mang/views.py
+++ Djangoproj/PurNet/course_mang/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
 from django.shortcuts import render_to_response
 from course_mang.models import *
 
@@ -19,3 +19,17 @@ def detail(request):
 
     return render_to_response('coursedetail.html', {'course':course})
 
+def add (request):
+    if request.method == 'POST':
+        title = request.POST.get('title', '')
+        school = request.POST.get('school', '')
+        num = request.POST.get('number', 0)
+        major = request.POST.get('major', '')
+        desc = request.POST.get('desc', '')
+        crn = request.POST.get('crn', '')
+
+        a = Course(course_school=school, course_major=major, course_title=title, course_number=num, course_CRN=crn, course_desc=desc)
+        a.save()
+        return HttpResponseRedirect('/courses/')
+    else :
+        return render_to_response('course_add.html', {'schooles':MAJOR_COLLEGES, 'majors':COURSE_MAJORS})
