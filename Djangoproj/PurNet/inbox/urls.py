91ed3916df612a10ed93e1f1c20036e9a06b0cac
Stephen Kercher <skercher@purdue.edu>
2015-04-30 00:37:00 -0400

    Fixed some errors

--- Djangoproj/PurNet/inbox/urls.py
+++ Djangoproj/PurNet/inbox/urls.py
@@ -2,9 +2,13 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns =[
     url(r'^$', 'inbox.views.inbox', name="inbox"),
-    url(r'^sent/$', 'inbox.views.sent', name="sent"),
-    url(r'^trash/$', 'inbox.views.trash', name="trash"),
+    url(r'^sent/', 'inbox.views.sent', name="sent"),
+    url(r'^trash/', 'inbox.views.trash', name="trash"),
     url(r'^compose/$', 'inbox.views.compose', name="compose"),
     url(r'^inbox/$', 'inbox.views.rcvd', name="inbox"),
+    url(r'^sent/?id=([0-9]+)/$' , 'inbox.views.msg_view', name="view_msg"),
+    url(r'^trash/?id=([0-9]+)/$' , 'inbox.views.msg_view', name="view_msg"),
+    url(r'^inbox/?id=([0-9]+)/$' , 'inbox.views.msg_view', name="view_msg"),
+
 
 ]
